93c919fc0495ae3926cee4ada8cade6676ded812
Helder Bargão Robalo <bargaorobalo@gmail.com>
2012-12-08 09:04:27 -0200

    Primeira interação. Adicionada a classe (GPL2) que cria a conexão serial
    Todo: Simplificar a class, fazendo com que a conexão serial seja feita
    com linux apenas com um exec().

---
+++ index.php
@@ -0,0 +1,48 @@
+﻿<html>
+<head>
+	<title>Alert my sales</title>
+	<META HTTP-EQUIV="Refresh" CONTENT="60">
+</head>
+<body>
+	<?php
+	$feed 		= file_get_contents('http://usuario:senha@dominio-feed.com.br/rss/order/new/'); //just put here your rss feed address
+	$rss 		= new SimpleXmlElement($feed);
+
+	$contador	= count($rss->channel->item);
+
+	$arquivo 	= 'contador.txt';
+	$ponteiro	= fopen($arquivo, 'r') or die("Erro1");
+	$quantidade	= fread($ponteiro, filesize($arquivo));
+	fclose($ponteiro);
+	
+	if($quantidade < $contador):
+		//emite o disparo do alarme
+		include "php-serial/php_serial.class.php";
+		$serial = new phpSerial;
+		$serial->deviceSet("COM6");
+		$serial->deviceOpen();
+		for ($i=0; $i <99 ; $i++):
+			$serial->sendMessage("Vendeu"); //Message to send to your device
+		endfor;
+		$serial->deviceClose();
+
+		//atualiza o arquivo
+		$ponteiro	= fopen($arquivo, 'w') or die("Erro");
+		fwrite($ponteiro, $contador);
+		fclose($ponteiro);
+
+		echo 'há uma nova venda: '; //feedback
+	else:
+		echo 'continuamos com esse total de vendas ';//feedback
+	endif;
+	echo $contador;
+	
+?>
+
+</body>
+</html>
+
+<?php
+// http://code.google.com/p/php-serial/ 
+
+?>
